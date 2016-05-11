52cbc9412e376422ea57b7ccb486c1967eea0ac4
Nathanael PINEAU <npineau@student.42.fr>
2016-05-11 13:51:14 +0200

    libft and norme

--- src/parser/parse_double.c
+++ src/parser/parse_double.c
@@ -1,21 +1,20 @@
 #include <stdlib.h>
 #include <string.h>
-//#include "libft"
 
-int parse_double(char* representation, double* value);
- 
-int parse_double(char* representation, double* value)
+int parse_double(char *representation, double *value);
+
+int	parse_double(char *representation, double *value)
 {
-    char*   safe;
+	char	*safe;
 
-    safe = NULL;
-    *value = strtod(representation, &safe);
-    if (safe == (representation + strlen(representation)))
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	safe = NULL;
+	*value = strtod(representation, &safe);
+	if (safe == (representation + strlen(representation)))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
